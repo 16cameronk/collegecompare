10e7614c5068baadcae6203f17f11ec8149430fa
16cameronk <hackathons@Macbook.home>
2016-03-23 23:45:30 -0400

    hi

---
+++ demo/scripts/all_products.js
@@ -0,0 +1,188 @@
+var allProducts = [
+    {
+        title: 'Bard College',
+        img: 'bard.png',
+        category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '32%',
+            'Rank': 'US News: #45 (Natl LAC)',
+            'Population': '2,051',
+            'Location': 'Annadale-on-Hudson, NY',
+        }
+    },
+    {
+        title: 'Columbia University',
+        img: 'columbia.jpg',
+      category: 'Ivy League',
+        subCategory: 'Medium Sized',
+        attributes: {
+            'Acceptance Rate': '6.1%',
+            'Rank': 'US News: #4 (Natl U) & #6 (Best Value)',
+            'Population': '6,170 (undergrad)',
+            'Location': 'New York, NY',
+            'Happiness Rate': '#31',
+            'Stress Rate': '#1'
+        }
+    },
+    {
+        title: 'Cornell University',
+        img: 'cornell.png',
+       category: 'Ivy League',
+        subCategory: 'Large Sized',
+        attributes: {
+            'Acceptance Rate': '14%',
+            'Rank': 'US News: #15 (Natl U) & #17 (Best Value)',
+            'Population': '27,589',
+            'Location': 'Ithaca, NY',
+            'Happiness Rate': '#35',
+            'Stress Rate': '#16',
+        }
+    },
+    {
+        title: 'Dartmouth College',
+        img: 'dartmouth.png',
+      category: 'Ivy League',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '10.3%',
+            'Rank': 'US News: #12 (Natl U) & #9 (Best Value)',
+            'Population': '6,298',
+            'Location': 'Hanover, NH',
+            'Happiness Rate': '#8',
+            'Stress Rate': '#13',
+        }
+    },
+    {
+        title: 'Duke University',
+        img: 'duke.png',
+       category: 'National Univerisity',
+        subCategory: 'Medium Sized',
+        attributes: {
+            'Acceptance Rate': '11.4%',
+            'Rank': 'US News: #8 (Natl U) & #7 (Best Value)',
+            'Population': '14,950 ',
+            'Location': 'Durham, NC', 
+            'Happiness Rate': '#18',
+            'Stress Rate': '#14',
+        }
+    },
+    {
+        title: 'Franklin & Marshall College',
+        img: 'fm.jpg',
+      category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '39%',
+            'Rank':'US News: #8 (Natl LAC) & #31 (Best Value)',
+            'Population': '2,209',
+            'Location': 'Lancaster, PA', 
+        }
+    },
+    {
+        title: 'Johns Hopkins University',
+        img: 'hopkins.jpg',
+        category: ' National University',
+        subCategory: 'Medium Sized',
+        attributes: {
+            'Acceptance Rate': '12.4%',
+            'Rank':'US News: #10 (Natl U) & #20 (Best Value)',
+            'Population': '6,469',
+            'Location': 'Baltimore, MD', 
+            'Happiness Rate': '#20',
+            'Stress Rate': '#11',
+            
+    }
+    },
+    {
+        title: 'Harvey Mudd College',
+        img: 'harveymudd.png',
+      category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '13.6%',
+            'Rank': 'US News: #14 (Natl LAC) & #1 (Engineering)',
+            'Population': '807',
+            'Location': 'Claremont, CA', 
+            'Happiness Rate': '#12'
+        }
+    },
+    
+    {
+        title: 'Rochester Institute of Technology',
+        img: 'rit.jpg',
+       category: 'Regional University',
+        subCategory: 'Medium Sized',
+        attributes: {
+            'Acceptance Rate': '57%',
+            'Rank': 'US News: #7 (NE Regionl U)',
+            'Population': '18,606',
+            'Location': 'Rochester, NY', 
+        }
+    },
+    {
+        title: 'Swarthmore College',
+        img: 'swarthmore.png',
+    category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '12%',
+            'Rank': 'US News: #3 (Natl LAC) & #8 (Best Value)',
+            'Population': '1,542',
+            'Location': 'Swarthmore, PA', 
+            'Happiness Rate': '#21', 
+            'Stress Rate': 'very stressful',
+        }
+    },
+    {
+        title: 'Trinity College',
+        img: 'trinity.png',
+       category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '30%',
+            'Rank': 'US News: #43 (Natl LAC) & #35 (Best Value)',
+            'Population': '1500',
+            'Location': 'Hartford, CT', 
+        }
+    },
+    {
+        title: 'Union College',
+        img: 'union.gif',
+     category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '41.1%',
+            'Rank': 'US News: #43 (Natl LAC)',
+            'Population': '1500',
+            'Location': 'Schenectady, NY', 
+        }
+    },
+    {
+        title: 'University of Miami',
+        img: 'miami.png',
+     category: 'National University',
+        subCategory: 'Medium Sized',
+        attributes: {
+            'Acceptance Rate': '40.5%',
+            'Rank': 'US News: #43 (Natl U)',
+            'Population': '16,774',
+            'Location': 'Coral Gables, FL', 
+            'Stress Rate': '#41',
+        }
+    },
+    {
+        title: 'Wesleyan University',
+        img: 'wesleyan.png',
+     category: 'Liberal Arts',
+        subCategory: 'Small Sized',
+        attributes: {
+            'Acceptance Rate': '21.9%',
+            'Rank': 'US News: #14 (Natl LAC) & #17 (Best Value)',
+            'Population': '2,906',
+            'Location': 'Middletown, Connecticut', 
+            'Happiness Rate': '#48'
+        }
+    },
+    
+];
